90a28adc832e6a0a4a9958ae70362560e7a195a7
destinysam <sams44802@gmail.com>
2020-09-15 11:01:25 +0530

    remove

--- flask/Main/forms.py
+++
@@ -1,6 +0,0 @@
-from flask_wtf import FlaskForm
-from flask_login import current_user
-from flask_wtf.file import FileField,FileAllowed
-from wtforms.fields import StringField,PasswordField,SubmitField,BooleanField,TextAreaField
-from wtforms.validators import DataRequired,Length,Email,EqualTo,ValidationError
-from Main.models import User
--- flask/run.py
+++
@@ -1,4 +0,0 @@
-from Main import create_app
-app = create_app()
-if __name__ == '__main__':
-	app.run(debug=True)
